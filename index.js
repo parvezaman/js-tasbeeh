e3691af6c0a9ca6340b4692ab4e7cac8e9620432
parvezaman <emailtoamanullah@gmail.com>
2021-08-24 05:52:45 +0600

    side bar added

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-var upCount= 0;
+const plusButton = document.getElementById('plus-button');
+const minusButton = document.getElementById('minus-button');
+const displayCount = document.getElementById('display-count');
 
-function countUp(upCount){
-    upCount++;
-    console.log(upCount);
-    return upCount;
-}
+
+let counter = 0;
+plusButton.addEventListener('click', () =>{
+    counter += 1;
+    displayCount.innerText = counter;
+    
+})
+
+minusButton.addEventListener('click', () =>{
+    if(counter>= 1){
+        counter -= 1;
+        displayCount.innerText = counter;
+    }
+})
