c85ead7361f88bf5b3bb92001c900eb19ae86eee
parvezaman <emailtoamanullah@gmail.com>
2021-08-24 09:53:26 +0600

    fixed counter

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 const plusButton = document.getElementById('plus-button');
 const minusButton = document.getElementById('minus-button');
 const displayCount = document.getElementById('display-count');
-
+const countLeft = document.getElementById('show-left');
+const wannaReadInput = document.getElementById('wanna-read-input');
+const wannaReadButton = document.getElementById('wanna-read-button');
+let iWantedToRead = 0;
 
 let counter = 0;
+wannaReadButton.addEventListener('click', () =>{
+    const iWannaRead = parseInt(wannaReadInput.value);
+    iWantedToRead = iWannaRead;
+    wannaReadInput.value = '';
+})
+
+
 plusButton.addEventListener('click', () =>{
+    // debugger
+    
     counter += 1;
     displayCount.innerText = counter;
-    
+    console.log(iWantedToRead);
+    countLeft.innerText = iWantedToRead-counter; 
+    if(counter == iWantedToRead){
+        countLeft.innerText = `done`;
+        plusButton.setAttribute('disabled', true);
+    }
 })
 
 minusButton.addEventListener('click', () =>{
     if(counter>= 1){
         counter -= 1;
         displayCount.innerText = counter;
+        countLeft.innerText = iWantedToRead-counter;
+        if(counter < iWantedToRead){
+            plusButton.removeAttribute('disabled', true);
+        }
     }
 })
+
